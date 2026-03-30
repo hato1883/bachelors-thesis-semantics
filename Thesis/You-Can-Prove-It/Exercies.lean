84d168b61b1f4fd1a115b6e5fef87f967a7f5c48
Hampus Toft <hampus.toft.1883@student.uu.se>
2026-03-30 12:43:08 +0200

    fix mathlib dep

--- Thesis/You-Can-Prove-It/Exercies.lean
+++ Thesis/You-Can-Prove-It/Exercies.lean
@@ -1,9 +1,13 @@
+import Mathlib.Data.Set.Basic
+
 inductive Person where
   -- left blank
 
 def is_scrub (p : Person) : Bool := sorry
+
 def tlc_members : Set Person := sorry
-def can_get_love_from (p1 p2 : Person) := sorry
+
+def can_get_love_from (p1 p2 : Person) : Bool := sorry
 
 def no_scrubs : Prop :=
-  \all p t, t \in tlc_members \and is_scrub p -> \not can_get_love_from t p
+  ∀ p t, t ∈ tlc_members ∧ is_scrub p → ¬ can_get_love_from t p
