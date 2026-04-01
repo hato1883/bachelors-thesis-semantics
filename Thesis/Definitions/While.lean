ff7ffb4455cb00bfb70ca29ef8c5f73a87becd1a
Hampus Toft <hampus.toft.1883@student.uu.se>
2026-04-01 10:40:00 +0200

    proof  progress

---
+++ Thesis/Definitions/While.lean
@@ -0,0 +1,134 @@
+
+import Mathlib
+
+namespace While
+
+  variable (n n₀ n₁ n₂ n₃ n₄ n₅ n₆ n₇ n₈ n₉ : Num)   --- n will range over numerals, Num.
+  variable (x x₀ x₁ x₂ x₃ x₄ x₅ x₆ x₇ x₈ x₉ : Var)   --- x will range over variables, Var.
+  variable (a a₀ a₁ a₂ a₃ a₄ a₅ a₆ a₇ a₈ a₉ : Aexp)  --- a will range over arithmetic expressions, Aexp.
+  variable (b b₀ b₁ b₂ b₃ b₄ b₅ b₆ b₇ b₈ b₉ : Bexp)  --- b will range over boolean expressions, Bexp.
+  variable (S S₀ S₁ S₂ S₃ S₄ S₅ S₆ S₇ S₈ S₉ : Stmt)  --- S will range over statements, Stmt.
+  variable (s s₀ s₁ s₂ s₃ s₄ s₅ s₆ s₇ s₈ s₉ : State) --- s will range over states, State.
+
+
+  inductive Num where
+    | zero : Num             -- 0
+    | one  : Num             -- 1
+    | succ0 (n : Num) : Num  -- n 0
+    | succ1 (n : Num) : Num  -- n 1
+
+  -- Helper: Converts a standard Nat to your bitvector Num
+  def natToNum : Nat → Num
+    | 0 => Num.zero
+    | 1 => Num.one
+    | n + 2 =>
+      if (n + 2) % 2 == 0
+      then Num.succ0 (natToNum ((n + 2) / 2))
+      else Num.succ1 (natToNum ((n + 2) / 2))
+
+  -- This instance allows you to use literals like '5' or '0b101' as Num
+  instance (n : Nat) : OfNat Num n where
+    ofNat := natToNum n
+
+  -- TODO: change to inductive instead of def so that proof 1.5 is needed
+  def Num_to_Z : Num → ℤ
+    | Num.zero    => 0
+    | Num.one     => 1
+    | Num.succ0 n => 2 * (Num_to_Z n)
+    | Num.succ1 n => 2 * (Num_to_Z n) + 1
+
+  notation "𝒩⟦" n "⟧" => Num_to_Z  n
+
+
+  /-- Variables are represented as strings of letters and digits -/
+  structure Var where
+    name : String
+    deriving BEq, Hashable, Repr, DecidableEq
+
+  instance : Coe String Var where
+    coe s := ⟨s⟩
+
+
+  /-- A State maps variables to their integer values -/
+  def State := Var → ℤ -- Gives notation s x = n, where (s : State) (x : Var) (n : Num)
+
+  def default_state : State := fun _ => 0
+
+  def assign (s : State) (x : Var) (z : ℤ) : State :=
+    fun (v: Var) => if v = x then z else s v
+
+  -- This allows you to write: s[x ↦ z]
+  notation:max s "[" x "↦" z "]" => assign s x z
+
+  --- arithmetic expressions
+  inductive Aexp where
+    | num (n : Num)          -- a := n
+    | var (x : Var)          -- a := x
+    | add (a₁ a₂ : Aexp)     -- a := a₁ + a₂
+    | mul (a₁ a₂ : Aexp)     -- a := a₁ ⋆ a₂
+    | sub (a₁ a₂ : Aexp)     -- a := a₁ - a₂
+
+  instance : Add Aexp where
+    add := Aexp.add
+
+  instance : Mul Aexp where
+    mul := Aexp.mul
+
+  instance : Sub Aexp where
+    sub := Aexp.sub
+
+  -- 1. Allow Strings to be treated as Aexp (via Var)
+  instance : Coe String Aexp where
+    coe s := Aexp.var ⟨s⟩
+
+  instance (n : Nat) : OfNat Aexp n where
+    ofNat := Aexp.num (OfNat.ofNat n)
+
+  /-- Semantic function for arithmetic expressions -/
+  -- TODO: change to inductive instead of def so that proof 1.8 is needed
+  def Aexp_eval : Aexp → State → ℤ
+    | Aexp.num n, _ => 𝒩⟦n⟧
+    | Aexp.var x, s => s x
+    | Aexp.add a₁ a₂, s => Aexp_eval a₁ s + Aexp_eval a₂ s
+    | Aexp.mul a₁ a₂, s => Aexp_eval a₁ s * Aexp_eval a₂ s
+    | Aexp.sub a₁ a₂, s => Aexp_eval a₁ s - Aexp_eval a₂ s
+
+  -- Traditional notation 𝒜⟦a⟧s → ℤ
+  notation "𝒜⟦" a "⟧" => Aexp_eval a
+
+
+  --- boolean expressions
+  inductive Bexp where
+    | true                   -- b := true
+    | false                  -- b := false
+    | eq  (a₁ a₂ : Aexp)     -- b := a₁ = a₂
+    | le  (a₁ a₂ : Aexp)     -- b := a₁ ≤ a₂
+    | not (b₁ : Bexp)        -- b := ¬b₁
+    | and (b₁ b₂ : Bexp)     -- b := b₁ ∧ b₂
+
+  instance : Coe Bool Bexp where
+    coe b := if b then Bexp.true else Bexp.false
+
+  /-- Semantic function for boolean expressions -/
+  -- TODO: change to inductive instead of def so that proof 1.8 is needed
+  def Bexp_eval : Bexp → State → Bool
+    | Bexp.true,      _ => true
+    | Bexp.false,     _ => false
+    | Bexp.eq  a₁ a₂, s => (Aexp_eval a₁ s) == (Aexp_eval a₂ s)
+    | Bexp.le  a₁ a₂, s => (Aexp_eval a₁ s) ≤  (Aexp_eval a₂ s)
+    | Bexp.not b₁,    s => ¬ (Bexp_eval b₁ s)
+    | Bexp.and b₁ b₂, s => (Bexp_eval b₁ s) ∧  (Bexp_eval b₂ s)
+
+  -- Traditional notation ℬ⟦b⟧s → Bool
+  notation "ℬ⟦" b "⟧" => Bexp_eval b
+
+
+  --- statements
+  inductive Stmt where
+    | ass  (x : Var)  (a : Aexp)     -- S = n
+    | skip                           -- S = x
+    | sequence        (S₁ S₂ : Stmt) -- S = S₁; S₂
+    | cond (b : Bexp) (S₁ S₂ : Stmt) -- S = if b then S₁ else S₂
+    | loop (b : Bexp) (S₁ : Stmt)    -- S = while b then S₁
+
+end While
---
+++ Thesis/playground/example-1-3.lean
@@ -0,0 +1,71 @@
+import Thesis.Definitions.While
+open While
+
+section part_1_3
+
+theorem n_total_mapping_z (n : While.Num) : ∃! (z : ℤ), 𝒩⟦n⟧ = z := by
+  induction n with
+  | zero =>
+    rw [While.Num_to_Z]
+    refine ⟨0, ?existence, ?uniqueness⟩
+    case existence =>
+      show 0 = 0
+      rfl
+    case uniqueness =>
+      intro y hy
+      symm
+      exact hy
+  | one =>
+    rw [While.Num_to_Z]
+    refine ⟨1, ?existence, ?uniqueness⟩
+    case existence =>
+      show 1 = 1
+      rfl
+    case uniqueness =>
+      intro y hy
+      symm
+      exact hy
+  | succ0 d hd =>
+    rw [While.Num_to_Z]
+    obtain ⟨z_d, h_prop, h_unique⟩ := hd
+    refine ⟨2 * z_d, ?existence, ?uniqueness⟩
+    case existence =>
+      show 2 * 𝒩⟦d⟧ = 2 * z_d
+      simp
+      exact h_prop
+    case uniqueness =>
+      intro y hy
+      symm
+      rw [h_prop] at hy
+      exact hy
+  | succ1 d hd =>
+    rw [While.Num_to_Z]
+    obtain ⟨z_d, h_prop, h_unique⟩ := hd
+    refine ⟨2 * z_d + 1, ?existence, ?uniqueness⟩
+    case existence =>
+      show 2 * 𝒩⟦d⟧ + 1 = 2 * z_d + 1
+      simp
+      exact h_prop
+    case uniqueness =>
+      intro y hy
+      symm
+      rw [h_prop] at hy
+      exact hy
+
+end part_1_3
+
+section part_1_6
+
+  def s : State := default_state["x"↦3]
+  #check 𝒜⟦"x" + 1⟧
+  #eval 𝒜⟦"x" + 1⟧ s
+
+end part_1_6
+
+
+section part_1_8
+
+  theorem aexp_total_mapping_z (a : Aexp) (s : State) : ∃! (z : ℤ), 𝒜⟦a⟧ s = z :=
+    by sorry
+
+end part_1_8
