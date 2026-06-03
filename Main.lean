eac768ed44b96dbcf766bf32f0d132c13b35e7eb
Hampus Toft <hampus.toft.1883@student.uu.se>
2026-06-03 11:01:17 +0200

    New Title page, Moved code to new repo

--- Main.lean
+++
@@ -1,4 +0,0 @@
-import Thesis
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
--- Thesis/playground/grind_test.lean
+++
@@ -1,5 +0,0 @@
-import Mathlib
--- Or more specifically, if available: import Lean.Meta.Tactic.Grind
-
-example (a b c : Nat) (h₁ : a = b) (h₂ : b = c) : a = c := by
-  grind
